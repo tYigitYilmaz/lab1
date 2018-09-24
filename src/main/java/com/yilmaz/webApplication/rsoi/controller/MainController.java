6baa8fbb6d0e0f47b148e52678aa18b69181895e
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-09-24 20:41:58 +0300

    Controller was added

---
+++ src/main/java/com/yilmaz/webApplication/rsoi/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.yilmaz.webApplication.rsoi.controller;
+
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/{name}")
+    public String helloUser(@PathVariable String name){
+        return "Hello " + name;
+    }
+}
