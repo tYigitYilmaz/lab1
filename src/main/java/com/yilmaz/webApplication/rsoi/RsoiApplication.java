8885d1baec82154e6cf6d3f78d10ae898119e69a
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-09-24 19:57:34 +0300

    the empty project was init

---
+++ src/main/java/com/yilmaz/webApplication/rsoi/RsoiApplication.java
@@ -0,0 +1,12 @@
+package com.yilmaz.webApplication.rsoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsoiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yilmaz/webApplication/rsoi/RsoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yilmaz.webApplication.rsoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsoiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
